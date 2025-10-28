30b04ae91ee0bb1d61d33dacdf7fcbf1e7929259
itoombes <124896022+itoombes@users.noreply.github.com>
2025-10-28 17:20:12 +1000

    Create ADNI class; generating test TensorDataset

--- recognition/ADNI-GFNet-48027782/dataset.py
+++ recognition/ADNI-GFNet-48027782/dataset.py
@@ -8,6 +8,41 @@ import re
 
 # Regular expression to match the ADNI file name format
 ADNI_PATTERN = re.compile(r"\d+_\d+.jpeg")
+TEST_PATH = 'AD_NC/test/'
+TRAIN_PATH = 'AD_NC/train/'
+CONTROL = 'NC/'
+PRESENT = 'AD/'
+
+class Adni():
+    def __init__(self, base_directory: str, validation_split: float = 0.2, validation_split_base: int | None = None):
+        '''
+        base_directory: Location of ADNI folder
+        validation_split: Amount of dataset to be split (rounds up)
+        validation_split_base: Random 
+        '''
+        self.directory = base_directory
+        self.validation_split = validation_split
+        self.validation_split_base = validation_split_base
+    
+    def get_test_data(self):
+        '''
+        Load the training dataset
+        '''
+        control = load_directory(self.directory+TEST_PATH+CONTROL)
+        present = load_directory(self.directory+TEST_PATH+PRESENT)
+        # Convert into tensors
+        control = torch.from_numpy(control)
+        present = torch.from_numpy(present)
+        # Create markers for positive and negative
+        control_out = torch.zeros(control.shape[0])
+        present_out = torch.ones(present.shape[0])
+        
+        # Stack together
+        images = torch.cat((control, present), dim=0)
+        output = torch.cat((control_out, present_out), dim=0)
+
+        # Load into dataset and return
+        return TensorDataset(images, output)
 
 def load_directory(directory) -> np.ndarray:
     '''
@@ -26,7 +61,7 @@ def load_directory(directory) -> np.ndarray:
                 key = int(file.split('_')[0])
                 # Read image, and convert into numpy array
                 im = np.asarray(Image.open(directory+file))
-    
+
                 data.append(im)
 
     # Convert to numpy array and return
@@ -58,7 +93,7 @@ def load_directory_by_id(directory) -> dict[int, np.ndarray]:
                 else:
                     id_grouped_data[key] = list()
                     id_grouped_data[key].append(im)
-    
+
     # Convert lists of images to numpy arrays
     for key in id_grouped_data:
         id_grouped_data[key] = np.stack(id_grouped_data[key])
@@ -67,9 +102,5 @@ def load_directory_by_id(directory) -> dict[int, np.ndarray]:
 
 
 if __name__ == "__main__":
-    ADNI_PATH = 'C:/Users/itoom/COMP3710/ADNI/'
-    TEST_PATH = ADNI_PATH+'AD_NC/test/'
-    TRAIN_PATH = ADNI_PATH+'AD_NC/train/'
-
-    print(load_directory(TEST_PATH+'AD/'))
-    print(load_directory_by_id(TEST_PATH+'AD/').keys())
+    adni = Adni('C:/Users/itoom/COMP3710/ADNI/')
+    adni.get_test_data()
