de63a16409cdbad588aeed77ea755f3ced43f148
itoombes <124896022+itoombes@users.noreply.github.com>
2025-10-28 15:00:36 +1000

    Generating an image containing the maximums of all samples to determine if cropping required

--- recognition/ADNI-GFNet-48027782/dataset.py
+++ recognition/ADNI-GFNet-48027782/dataset.py
@@ -2,7 +2,13 @@ from PIL import Image
 import os
 import numpy as np
 import pandas as pd
+import torch
 import re
+
+import matplotlib.pyplot as plt
+
+# Regular expression to match the ADNI file name format
+ADNI_PATTERN = re.compile(r"\d+_\d+.jpeg")
 
 def get_patient_ids(directory: str) -> set: 
     '''
@@ -15,15 +21,47 @@ def get_patient_ids(directory: str) -> set:
     '''
     ids = set()
 
-    # Regular expression to match the ADNI file name format
-    adni_pattern = re.compile(r"\d+_\d+.jpeg")
-
-
     if os.path.isdir(directory):
         # Iterate over every file in directory
         for file in os.listdir(directory):
             # Add the patient ID of any valid image to the set
-            if adni_pattern.match(file):
+            if ADNI_PATTERN.match(file):
                 ids.add(file.split('_')[0])
 
     return ids 
+
+def load_image(path: str) -> np.ndarray | None:
+    """
+    Load an image into a 2d tensor. Returns None if invalid path.
+    
+    path: image to load
+    """
+    if (os.path.exists(path)):
+        im = Image.open(path)
+        return np.asarray(im)
+
+if __name__ == "__main__":
+    ADNI_PATH = 'C:/Users/itoom/COMP3710/ADNI/'
+    TEST_PATH = ADNI_PATH+'AD_NC/test/'
+    TRAIN_PATH = ADNI_PATH+'AD_NC/train/'
+
+    # 30520 240 x 256 images
+    images = np.zeros((30520, 240, 256)) 
+    i = 0
+
+    for path in (TEST_PATH+'AD/', TEST_PATH+'NC/', TRAIN_PATH+'AD/', TRAIN_PATH+'NC/'):
+        for i, file in enumerate(os.listdir(path)):
+            im = load_image(path+file)
+            if im is not None:
+                images[i] = im
+                i += 1
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    images = torch.from_numpy(images)
+    images.to(device)
+    print("Determining maximum...")
+    image_max = torch.max(images, dim=0).values
+
+    plt.imshow(image_max.cpu().numpy(), cmap='gray')
+    plt.show()
