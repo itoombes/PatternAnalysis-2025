43b6dfba388e99308d39629c8c14ae33a3a36c18
itoombes <124896022+itoombes@users.noreply.github.com>
2025-10-28 23:04:08 +1000

    Implemented MLP from original gfnet.py

--- recognition/ADNI-GFNet-48027782/modules.py
+++ recognition/ADNI-GFNet-48027782/modules.py
@@ -6,7 +6,8 @@ from timm.layers.drop import DropPath
 import math
 
 '''
-REFERENCE: GFNet/gfnet.py
+Many of the classes within have essentially been transferred from GFNet.py,
+with some additional documentation.
 
 Y. Rao, W. Zhau, Z. Zhu, Z. Zhou, and J. Lu
 Global Filter Networks for Image Classification, 2021
@@ -57,7 +58,26 @@ class GlobalFilter(nn.Module):
 class MultiLayerPerceptron(nn.Module):
     '''
     Multilayer Perceptron
-    Directly copied from GFNet Git
+    
+    Two fully connected layers, with an activation function between them
+    Uses the same dropout on both layers.
+    Defaults to Gaussian Error Linear Unit.
     '''
-    def __init__(self, in_features, hidden_features=None, out_features=None):
-        pass
+    def __init__(self, in_features, hidden_features = None, out_features = None,
+                 act_layer = nn.GELU, drop=0.0):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
